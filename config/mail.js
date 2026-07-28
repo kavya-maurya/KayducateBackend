29affe4c2d9f18369da3217082732f43f492b31f
kavya <kavyamaurya269@gmail.com>
2026-07-28 12:26:47 +0530

    mail

--- config/mail.js
+++ config/mail.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
+const smtpUser = process.env.SMPT_USER || process.env.EMAIL_USER;
+const smtpPass = process.env.SMPT_PW || process.env.EMAIL_PASS;
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: process.env.SMTP_HOST || "smtp-relay.brevo.com",
+    port: Number(process.env.SMTP_PORT || 587),
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        user: smtpUser,
+        pass: smtpPass,
+    },
 });
 
 module.exports = transporter;
